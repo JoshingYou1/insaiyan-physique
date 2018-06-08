fb76894fc7d664307892b4d0d642367c57d768e9
Joshua Drumm <joshing_you1@yahoo.com>
2018-06-08 15:07:20 -0400

    API functions created in JS

--- index.js
+++ index.js
@@ -2,40 +2,23 @@
 
 const WORKOUT_BASE_URL = "https://wger.de/api/v2/";
 const WORKOUT_URLS = {
-    workout: WORKOUT_BASE_URL + "workout",         
-    workoutsession: WORKOUT_BASE_URL + "workoutsession",
-    schedulestep: WORKOUT_BASE_URL + "schedulestep",
-    schedule: WORKOUT_BASE_URL + "schedule",
-    day: WORKOUT_BASE_URL + "day",
-    set: WORKOUT_BASE_URL + "set",
-    setting: WORKOUT_BASE_URL + "setting",
-    workoutlog: WORKOUT_BASE_URL + "workoutlog",
-    userprofile: WORKOUT_BASE_URL + "userprofile",
-    language: WORKOUT_BASE_URL + "language",
-    daysofweek: WORKOUT_BASE_URL + "daysofweek",
-    license: WORKOUT_BASE_URL + "license",
-    repetitionunit: WORKOUT_BASE_URL + "setting-repetitionunit",
-    weightunit: WORKOUT_BASE_URL + "setting-weightunit",
+            
     exerciseinfo: WORKOUT_BASE_URL + "exerciseinfo",
     exercise: WORKOUT_BASE_URL + "exercise",
     equipment: WORKOUT_BASE_URL + "equipment",
     exercisecategory: WORKOUT_BASE_URL + "exercisecategory",
     exerciseimage: WORKOUT_BASE_URL + "exerciseimage",
     exercisecomment: WORKOUT_BASE_URL + "exercisecomment",
-    muscle: WORKOUT_BASE_URL + "muscle",
-    ingredient: WORKOUT_BASE_URL + "ingredient",
-    weightunit: WORKOUT_BASE_URL + "weightunit",
-    ingredientweightunit: WORKOUT_BASE_URL + "ingredientweightunit",
-    nutritionplan: WORKOUT_BASE_URL + "nutritionplan",
-    meal: WORKOUT_BASE_URL + "meal",
-    mealitem: WORKOUT_BASE_URL + "mealitem",
-    weightentry: WORKOUT_BASE_URL + "weightentry"
+    muscle: WORKOUT_BASE_URL + "muscle"
 }
 
-function retrieveDataFromApi(searchTerm, callback) {
+function retrieveExercisesFromApi(exercise, callback) {
     const settings = {
-        url: WORKOUT_SEARCH_URL,
-        q: `${searchTerm}`,
+        url: WORKOUT_URLS.exercise,
+        data: {
+            license_author: "wger.de",
+            language: 2
+        },
         dataType: "json",
         type: "GET",
         success: callback
@@ -43,3 +26,141 @@ function retrieveDataFromApi(searchTerm, callback) {
 
     $.ajax(settings);
 }
+
+function retrieveExerciseInfoFromApi(exerciseId, callback) {
+    const settings = {
+        url: WORKOUT_URLS.exerciseinfo + "/" + exerciseId,
+        data: {
+            license_author: "wger.de",
+            language: 2,
+            exercise: exerciseId
+        },
+        dataType: "json",
+        type: "GET",
+        success: callback
+    };
+
+    $.ajax(settings);
+}
+
+function retrieveEquipmentFromApi(exerciseId, callback) {
+    const settings = {
+        url: WORKOUT_URLS.equipment,
+        data: {
+            license_author: "wger.de",
+            language: 2,
+            exercise: exerciseId
+        },
+        dataType: "json",
+        type: "GET",
+        success: callback
+    };
+
+    $.ajax(settings);
+}
+
+function retrieveMuscleCategoryFromApi(callback) {
+    const settings = {
+        url: WORKOUT_URLS.exercisecategory,
+        data: {
+            license_author: "wger.de",
+            language: 2
+        },
+        dataType: "json",
+        type: "GET",
+        success: callback
+    };
+
+    $.ajax(settings);
+}
+
+function retrieveExerciseImgFromApi(exerciseId, callback) {
+    const settings = {
+        url: WORKOUT_URLS.exerciseimage,
+        data: {
+            license_author: "wger.de",
+            language: 2,
+            exercise: exerciseId
+        },
+        dataType: "json",
+        type: "GET",
+        success: callback
+    };
+
+    $.ajax(settings);
+}
+
+function retrieveExerciseCommentFromApi(exerciseId, callback) {
+    const settings = {
+        url: WORKOUT_URLS.exercisecomment,
+        data: {
+            license_author: "wger.de",
+            language: 2,
+            exercise: exerciseId
+        },
+        dataType: "json",
+        type: "GET",
+        success: callback
+    };
+
+    $.ajax(settings);
+}
+
+function retrieveMuscleInfoFromApi(exerciseId, callback) {
+    const settings = {
+        url: WORKOUT_URLS.muscle,
+        data: {
+            license_author: "wger.de",
+            language: 2
+        },
+        dataType: "json",
+        type: "GET",
+        success: callback
+    };
+
+    $.ajax(settings);
+}
+
+function debug(data) {
+    console.log(data);
+}
+
+retrieveMuscleCategoryFromApi(debug);
+
+
+
+function renderMuscleCategoryButtons(data) {
+    let buttonTemplate = "";
+    data.results.forEach(muscleGroup => {
+        buttonTemplate += `<button>${muscleGroup.name}</button>`;
+    });
+    const muscleCategoryPage = `
+        <section class="muscle-group-page">
+            <h2>Choose which muscle group you would like to focus on:</h2>
+
+            ${buttonTemplate}
+
+
+        </section>`;
+
+        $("#container").html(muscleCategoryPage);
+}
+
+function startPageSubmitButton() {
+    $("#container").on("click", ".initialize-app-button", function(event) {
+        event.preventDefault();
+
+        retrieveMuscleCategoryFromApi(renderMuscleCategoryButtons);
+    });
+
+}
+
+function getExercisesByMuscleGroup(categoryId, callback) {
+ 
+}
+
+ function fascade() {
+    startPageSubmitButton();
+ }
+
+ $(fascade);
