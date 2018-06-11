e2d65b20c0f438ebb88c5ae95c5156f9ebcafb5a
Joshua Drumm <joshing_you1@yahoo.com>
2018-06-11 11:40:11 -0400

    Updated js with new buttons and methods

--- index.js
+++ index.js
@@ -12,12 +12,13 @@ const WORKOUT_URLS = {
     muscle: WORKOUT_BASE_URL + "muscle"
 }
 
-function retrieveExercisesFromApi(exercise, callback) {
+function retrieveExercisesByCategoryIdFromApi(categoryId, callback) {
     const settings = {
         url: WORKOUT_URLS.exercise,
         data: {
             license_author: "wger.de",
-            language: 2
+            language: 2,
+            category: categoryId 
         },
         dataType: "json",
         type: "GET",
@@ -42,6 +43,7 @@ function retrieveExerciseInfoFromApi(exerciseId, callback) {
 
     $.ajax(settings);
 }
+
 
 function retrieveEquipmentFromApi(exerciseId, callback) {
     const settings = {
@@ -125,25 +127,36 @@ function debug(data) {
     console.log(data);
 }
 
-retrieveMuscleCategoryFromApi(debug);
+//retrieveMuscleCategoryFromApi(debug);
 
+var currentMuscleCategory = "";
 
+const MUSCLE_IMAGE_MAP = {
+    8: "arms.image-path",
+    9: "legs.image-path",
+    10: "abs.image-path",
+    11: "chest.jpg",
+    12: "back.image-path",
+    13: "shoulders.image-path",
+    14: "calves.image-path"
+}
 
 function renderMuscleCategoryButtons(data) {
     let buttonTemplate = "";
-    data.results.forEach(muscleGroup => {
-        buttonTemplate += `<button>${muscleGroup.name}</button>`;
+    data.results.forEach(muscleCategory => {
+        buttonTemplate += `<button data-category-id="${muscleCategory.id}" data-category-name="${muscleCategory.name}" class="muscle-category-button">
+            <img src="Muscle_Images/${MUSCLE_IMAGE_MAP[muscleCategory.id]}"/>
+        </button>`;
     });
     const muscleCategoryPage = `
-        <section class="muscle-group-page">
-            <h2>Choose which muscle group you would like to focus on:</h2>
+        <section class="muscle-category-page">
+            <h2>Choose which muscle category you would like to focus on:</h2>
 
             ${buttonTemplate}
 
-
         </section>`;
 
-        $("#container").html(muscleCategoryPage);
+    $("#container").html(muscleCategoryPage);
 }
 
 function startPageSubmitButton() {
@@ -155,12 +168,54 @@ function startPageSubmitButton() {
 
 }
 
-function getExercisesByMuscleGroup(categoryId, callback) {
- 
+function renderExercisesByMuscleCategory(data) {
+    let listTemplate = "";
+    data.results.forEach(exercise => {
+        //TO-DO: Add a link to make the li clickable(<a href="javascript:void(0);"data-exercise-id="*id*" class="exercise-link">)
+        listTemplate += `<li>${exercise.name}</li>`;
+    })
+
+    const exerciseListPage = `
+        <section class="exercise-list-page">
+            <h2>Recommended Exercises for ${currentMuscleCategory}</h2>
+            <button class="back-to-muscle-category-page-button">Back to Muscle Categories</button>
+            <h3>Choose any of the following exercises for additional information, including repitition and set ranges:<h2>
+
+            <ul class="exercise-list">
+                ${listTemplate}
+            </ul>    
+
+        </section>`;
+
+    $("#container").html(exerciseListPage);
 }
 
- function fascade() {
+function muscleCategoryPageSubmitButtons(renderMuscleCategoryButtons) {
+    $("#container").on("click", ".muscle-category-button", function(event) {
+        event.preventDefault();
+
+        currentMuscleCategory = $(event.currentTarget).data("category-name");
+
+        retrieveExercisesByCategoryIdFromApi($(event.currentTarget).data("category-id"), renderExercisesByMuscleCategory);
+    });
+}
+
+function backToMuscleCategoryButton() {
+    $("#container").on("click", ".back-to-muscle-category-page-button", function(event) {
+        event.preventDefault();
+
+        retrieveMuscleCategoryFromApi(renderMuscleCategoryButtons);
+    })
+}
+
+function renderExerciseInfoPage(data) {
+
+}
+
+function handleSubmitButtons() {
     startPageSubmitButton();
+    muscleCategoryPageSubmitButtons();
+    backToMuscleCategoryButton();
  }
 
- $(fascade);
+ $(handleSubmitButtons);
